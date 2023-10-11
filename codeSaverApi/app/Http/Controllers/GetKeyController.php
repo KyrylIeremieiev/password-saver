1add0e0b90d16eb29a9e4fbd9d7ebe1806cc9b83
KyrylIeremieiev <34677@ma-web.nl>
2023-10-11 22:57:42 +0200

    add put code, make getKeyModel

--- codeSaverApi/app/Http/Controllers/GetKeyController.php
+++ codeSaverApi/app/Http/Controllers/GetKeyController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\getKeyModel;
 class GetKeyController extends Controller
 {
     public function get($id){
-
         
     }
 }
--- codeSaverApi/app/Http/Controllers/PutKeyController.php
+++ codeSaverApi/app/Http/Controllers/PutKeyController.php
@@ -7,5 +7,18 @@ use Illuminate\Support\Facades\DB;
 class PutKeyController extends Controller
 {
     public function insert(Request $request, $id){
+        $validatedData = $request->validate([
+            'id' => 'required|string',
+            'value' => 'required|string',
+            // Add more validation rules as needed
+        ]);
+
+        DB::table('pass')->insert([
+            'id' => $validatedData["id"],
+            'value' => $validatedData['value']
+            // Add more columns and values as needed
+        ]);
+
+        return $validatedData;
     }
 }
